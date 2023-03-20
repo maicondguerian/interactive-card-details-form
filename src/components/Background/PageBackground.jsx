4dda46c8b5959cead1cf8a7557909c88554cd096
maicondguerian <maicondguerian@gmail.com>
2023-03-20 15:41:04 -0300

    styling

--- src/components/Background/PageBackground.jsx
+++ src/components/Background/PageBackground.jsx
@@ -1,9 +1,14 @@
 import styles from "./pageBackground.module.scss";
+import cardFrontImg from '../../assets/bg-card-front.png'
+
+const CardFront = () =>{
+  return <img src={cardFrontImg} alt="cardFront" />
+}
 
 export const PageBackground = ( {children} ) => {
   return (
     <div id={styles.BackMain}>
-      <div id={styles.LeftPageBackground}></div>
+      <div id={styles.LeftPageBackground}> <CardFront /> </div>
       <div id={styles.RightPageBackground}> {children} </div>
     </div>
   );
