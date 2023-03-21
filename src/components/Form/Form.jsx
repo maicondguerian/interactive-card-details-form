32634d5a803d66051c30b0a49c91b06115496b29
maicondguerian <maicondguerian@gmail.com>
2023-03-21 01:08:56 -0300

    edtl

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,25 +5,24 @@ export const Form = () => {
 
         <div className={styles.formWrapper}>
             <form className={styles.form}>
-                <fieldset id={styles.nomeNumero}>
+                <fieldset id={styles.cardInfo}>
                         <label for="name">cardholder name</label>
-                        <input type="text" id='name' required pattern='[a-z]+' placeholder='e.g. Jane Applessed' />
+                        <input type="text" id='name' required pattern='[a-z]+' placeholder='e.g. Jane Applessed' maxlength="50"/>
                         <label for="number">card number</label>
-                        <input type="text" id='number' required pattern='[0-9]{12}' placeholder='e.g. 1234 5678 9123 0000'/>
+                        <input type="number" id='number' required pattern='[0-9]{12}' placeholder='e.g. 1234 5678 9123 0000'  min="0" max="12"/>
                 </fieldset>
                 <fieldset className={styles.dateCvc}>
                     <div className={styles.data}>
-                        <label htmlFor="month year">exp.date (mm/yy)</label>
+                        <label htmlFor="month">exp.date (mm/yy)</label>
                         <div className={styles.dataDado}>
-                            <input type="text" name="" id="month" required pattern='[0-9]{2}' placeholder='MM'/>
-                            <input type="text" name="" id="year" required pattern='[0-9]{2}' placeholder='YY'/>
+                            <input type="number" name="" id="month" maxlength="2" required pattern='[0-9]{2}' placeholder='MM'/>
+                            <input type="number" name="" id="year" maxlength="2" required pattern='[0-9]{2}' placeholder='YY'/>
                         </div>
                     </div>
                     <div className={styles.cvc}>
                         <label htmlFor="cvc">cvc</label>
-                        <input type="text" name="" id="cvc" required pattern='[0-9]{3}' placeholder='e.g. 123'/>
+                        <input type="number" name="" id="cvc" maxlength="3" required pattern='[0-9]{3}' placeholder='e.g. 123'/>
                     </div>
-                
                 </fieldset>
             <button type='submit'>Confirm</button>
             </form>
