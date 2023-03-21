1c657c06aa9c37d076ea8e93459b13e0c4e221ec
maicondguerian <maicondguerian@gmail.com>
2023-03-20 22:27:30 -0300

    creating form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,29 +4,31 @@ export const Form = () => {
     return (
 
         <div className={styles.formWrapper}>
-            <ul>
-                <li>
-                    <p><label for="name">cardholder name</label></p>
-                    <span>
-                        <input type="text" id='name' />
-                    </span>
-                </li>
-                <li>
-                    <p><label for="number">card number</label></p>
-                    <span>
-                        <input type="text" id='number' />
-                    </span>
-                </li>
-            </ul>
-            <span>EXP. DATE (MM/YY)</span>
-            <div >
-                <ul className={styles.secondUl}>
-                <li><input type="text" name="" id="month" /></li>
-                <li><input type="text" name="" id="" /></li>
-                <li><input type="text" name="" id="" /></li>
+            <div className={styles.form}>
+                <ul id={styles.ul}>
+                    <li>
+                        <p><label for="name">cardholder name</label></p>
+                        <span>
+                            <input type="text" id='name' />
+                        </span>
+                    </li>
+                    <li>
+                        <p><label for="number">card number</label></p>
+                        <span>
+                            <input type="text" id='number' />
+                        </span>
+                    </li>
                 </ul>
-            </div>
+                <span>EXP. DATE (MM/YY)</span>
+                <div>
+                    <ul className={styles.secondUl}>
+                        <li><input type="text" name="" id="month" /></li>
+                        <li><input type="text" name="" id="" /></li>
+                        <li><input type="text" name="" id="" /></li>
+                    </ul>
+                </div>
             <button>Confirm</button>
+            </div>
         </div>
 
 
